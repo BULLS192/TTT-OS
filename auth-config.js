f3b147b11f43215076df880a9d50af6e19d9d8e4
BULLS192 <kevinbullsyap@gmail.com>
2026-09-12 18:16:01 -0500

    Add TTT OS authentication configuration scaffold

---
+++ auth-config.js
@@ -0,0 +1,8 @@
+window.TTT_AUTH_CONFIG = Object.freeze({
+  enabled: false,
+  supabaseUrl: '',
+  publishableKey: '',
+  redirectPath: '/',
+  appName: 'TTT OS',
+  supportText: 'Authorized Thompson Transportation Technologies personnel only.'
+});
