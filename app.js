e31554ad266c1ea12c1a0c670d1f7f41f79ba8f0
BULLS192 <kevinbullsyap@gmail.com>
2026-09-16 13:21:14 -0500

    Load TTT Expenses module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // TTT OS bootstrap: apply the approved master branding first, then load the original app and additive modules synchronously.
 (function(){
-  const v='20260914-scheduling-v11';
+  const v='20260916-expenses-v1';
   const favicon='/favicon-64.png?v='+v;
   document.querySelectorAll('link[rel="icon"],link[rel="shortcut icon"]').forEach((el)=>el.remove());
   const icon=document.createElement('link');
@@ -27,4 +27,5 @@
   document.write('<script src="/scheduling-v09.js?v='+v+'"></'+'script>');
   document.write('<script src="/scheduling-v10.js?v='+v+'"></'+'script>');
   document.write('<script src="/scheduling-v11.js?v='+v+'"></'+'script>');
+  document.write('<script src="/expenses-v01.js?v='+v+'"></'+'script>');
 })();
