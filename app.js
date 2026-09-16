71ccb4fccc887fb659540ea0e24e25de07c6628e
BULLS192 <kevinbullsyap@gmail.com>
2026-09-16 17:23:55 -0500

    Deploy rebuilt stable personnel editor

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // TTT OS bootstrap: apply the approved master branding first, then load the original app and additive modules synchronously.
 (function(){
-  const v='20260916-personnel-v4';
+  const v='20260916-personnel-v5';
   const favicon='/favicon-64.png?v='+v;
   document.querySelectorAll('link[rel="icon"],link[rel="shortcut icon"]').forEach((el)=>el.remove());
   const icon=document.createElement('link');
@@ -30,7 +30,5 @@
   document.write('<script src="/expenses-v01.js?v='+v+'"></'+'script>');
   document.write('<script src="/expenses-v02.js?v='+v+'"></'+'script>');
   document.write('<script src="/expenses-v03.js?v='+v+'"></'+'script>');
-  document.write('<script src="/personnel-v01.js?v='+v+'"></'+'script>');
-  document.write('<script src="/personnel-v03.js?v='+v+'"></'+'script>');
-  document.write('<script src="/personnel-v04.js?v='+v+'"></'+'script>');
+  document.write('<script src="/personnel-v05.js?v='+v+'"></'+'script>');
 })();
