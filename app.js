18728ec1d0c0b963cf980d48bff1b0ef69d5a998
BULLS192 <kevinbullsyap@gmail.com>
2026-09-16 16:37:08 -0500

    Load personnel dropdown fix and Amjad seed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // TTT OS bootstrap: apply the approved master branding first, then load the original app and additive modules synchronously.
 (function(){
-  const v='20260916-personnel-v2';
+  const v='20260916-personnel-v3';
   const favicon='/favicon-64.png?v='+v;
   document.querySelectorAll('link[rel="icon"],link[rel="shortcut icon"]').forEach((el)=>el.remove());
   const icon=document.createElement('link');
@@ -32,4 +32,5 @@
   document.write('<script src="/expenses-v03.js?v='+v+'"></'+'script>');
   document.write('<script src="/personnel-v01.js?v='+v+'"></'+'script>');
   document.write('<script src="/personnel-v02.js?v='+v+'"></'+'script>');
+  document.write('<script src="/personnel-v03.js?v='+v+'"></'+'script>');
 })();
