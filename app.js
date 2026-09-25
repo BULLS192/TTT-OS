61dcf4e9f61947506e969155ba71391a3a624eff
BULLS192 <kevinbullsyap@gmail.com>
2026-09-25 02:00:15 -0500

    Load CRM ERP cockpit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // TTT OS bootstrap: apply the approved master branding first, then load the original app and additive modules synchronously.
 (function(){
-  const v='20260925-media-v4';
+  const v='20260925-crm-erp-v1';
   const favicon='/favicon-64.png?v='+v;
   document.querySelectorAll('link[rel="icon"],link[rel="shortcut icon"]').forEach((el)=>el.remove());
   const icon=document.createElement('link');
@@ -27,7 +27,7 @@
   document.write('<script src="/cloud-sync.js?v='+v+'"></'+'script>');
   document.write('<script src="/core-relational-v05.js?v='+v+'"></'+'script>');
   document.write('<script src="/media-cloud-v06.js?v='+v+'"></'+'script>');
-  document.write('<script src="/operator-ui.js?v='+v+'"></'+'script>');
+  document.write('<script src="/operator-ui.js?v='+v+'"></'+'script>');\n  document.write('<script src="/crm-erp-v13.js?v='+v+'"></'+'script>');
   document.write('<script src="/vendors-v08.js?v='+v+'"></'+'script>');
   document.write('<script src="/scheduling-core-v09.js?v='+v+'"></'+'script>');
   document.write('<script src="/scheduling-v09.js?v='+v+'"></'+'script>');
