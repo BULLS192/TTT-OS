13deb9d22f5d0b6d9a1708ed468d2069f377fa0e
BULLS192 <kevinbullsyap@gmail.com>
2026-09-25 02:28:12 -0500

    Load Supabase ERP product master adapter

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // TTT OS bootstrap: apply the approved master branding first, then load the original app and additive modules synchronously.
 (function(){
-  const v='20260925-ops-relational-v13';
+  const v='20260925-erp-product-master-v14';
   const favicon='/favicon-64.png?v='+v;
   document.querySelectorAll('link[rel="icon"],link[rel="shortcut icon"]').forEach((el)=>el.remove());
   const icon=document.createElement('link');
@@ -40,4 +40,5 @@
   document.write('<script src="/expenses-v03.js?v='+v+'"></'+'script>');
   document.write('<script src="/personnel-v05.js?v='+v+'"></'+'script>');
   document.write('<script src="/product-catalog-v12.js?v='+v+'"></'+'script>');
+  document.write('<script src="/erp-product-master-v14.js?v='+v+'"></'+'script>');
 })();
